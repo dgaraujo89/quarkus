e338f23d91945731720933390b39ca06894731e7
Diego Araujo <diegogomesaraujo@gmail.com>
2021-10-27 20:12:12 -0300

    script build test

--- hello-world/ops/build.sh
+++ hello-world/ops/build.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-env | grep GO_
+mvn clean package
+
+
+#GO_TO_REVISION_REPO=872f7e4433f66ee84bd3e3294f0edf0c8e9d17c0
+#GO_FROM_REVISION_REPO=872f7e4433f66ee84bd3e3294f0edf0c8e9d17c0
+#GO_PIPELINE_NAME=quarkus-hello-world-build
+#GO_STAGE_COUNTER=1
+#GO_MATERIAL_REPO_HAS_CHANGED=true
+#GO_TRIGGER_USER=anonymous
+#GO_PIPELINE_GROUP_NAME=quarkus-hello-world
+#GO_AGENT_RESOURCES=docker,maven
+#GO_MATERIAL_BRANCH_REPO=master
+#GO_REVISION_REPO=872f7e4433f66ee84bd3e3294f0edf0c8e9d17c0
+#GO_STAGE_NAME=build
+#GO_JOB_NAME=build
+#GO_PIPELINE_COUNTER=2
+#GO_SERVER_URL=http://localhost:8153/go
+#GO_PIPELINE_LABEL=2
+#GO_MATERIAL_URL_REPO=https://github.com/diegogomesaraujo/quarkus.git
